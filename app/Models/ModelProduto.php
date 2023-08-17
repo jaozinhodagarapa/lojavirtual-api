f7b3310aa5127c764fcff364901b653597fef785
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-08-17 15:27:48 -0300

    .

---
+++ app/Models/ModelProduto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelProduto extends Model
+{
+    use HasFactory;
+
+    
+    protected $fillable = [
+        'produto',
+        'codigo',
+        'preço',
+        'tipo',
+        'linha'
+    ];
+}
